b5a0e3a9e70f8b899e720b00f2bbf6e67ca969c9
WorKSimi <116308823+WorKSimi@users.noreply.github.com>
2025-02-17 18:47:06 -0500

    AngularTestProject

---
+++ src/app/components/client/client.component.ts
@@ -0,0 +1,78 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Client } from '../../model/class/Client';
+import { FormsModule } from '@angular/forms';
+import { ClientService } from '../../services/client.service';
+import { APIResponseModel } from '../../model/interface/role';
+import { AsyncPipe, DatePipe, JsonPipe, UpperCasePipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { AlertComponent } from "../../reusableComponent/alert/alert.component";
+import { MyButtonComponent } from "../../reusableComponent/my-button/my-button.component";
+
+@Component({
+  selector: 'app-client',
+  imports: [FormsModule, UpperCasePipe, DatePipe, JsonPipe, AsyncPipe, AlertComponent, MyButtonComponent],
+  templateUrl: './client.component.html',
+  styleUrl: './client.component.css'
+})
+export class ClientComponent implements OnInit 
+{
+
+  currentDate: Date = new Date();
+
+  clientObj: Client = new Client();
+  clientList: Client[] = [];
+
+  clientService = inject(ClientService)
+
+  userList$: Observable<any> = new Observable<any>;
+
+  ngOnInit(): void 
+  {
+    this.loadClient();
+    this.userList$ = this.clientService.getAllUser();
+  }
+
+  loadClient()
+  {
+    this.clientService.getAllClients().subscribe((res:APIResponseModel)=>{
+      this.clientList = res.data;
+    })
+  }
+
+  onSaveClient(data: string) {
+    debugger;
+    this.clientService.addUpdate(this.clientObj).subscribe((res:APIResponseModel)=>{
+      if(res.result) {
+        alert("Client Created Success")
+        this.loadClient();
+        this.clientObj = new Client();
+      } else {
+        alert(res.message)
+      }
+    })
+  }
+
+  onEdit(data: Client)
+  {
+    this.clientObj = data;
+  }
+
+  onDelete(id: number) {
+
+    const isDelete = confirm("Are you sure you want to delete");
+    if (isDelete)
+    {
+      this.clientService.deletClientById(id).subscribe((res:APIResponseModel)=>{
+        if(res.result) 
+        {
+          alert("Client Delete Success")
+          this.loadClient();
+        }
+        else
+        {
+          alert(res.message)
+        }
+      })
+    }
+  }
+}
